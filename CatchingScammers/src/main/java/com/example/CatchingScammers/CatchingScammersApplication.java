c999ede9da99d1348be90143033738a32386f5ac
revoly afanasev <rewoly@mail.ru>
2024-10-31 21:26:03 +0900

    first commit

---
+++ CatchingScammers/src/main/java/com/example/CatchingScammers/CatchingScammersApplication.java
@@ -0,0 +1,13 @@
+package com.example.CatchingScammers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatchingScammersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatchingScammersApplication.class, args);
+	}
+
+}
---
+++ CatchingScammers/src/test/java/com/example/CatchingScammers/CatchingScammersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CatchingScammers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatchingScammersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
