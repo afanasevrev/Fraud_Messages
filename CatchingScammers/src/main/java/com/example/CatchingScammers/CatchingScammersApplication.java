aede333924f6d6c9181c865308493c3621e97946
revoly afanasev <rewoly@mail.ru>
2024-10-31 21:35:49 +0900

    Начало

--- CatchingScammers/src/main/java/com/example/CatchingScammers/CatchingScammersApplication.java
+++ CatchingScammers/src/main/java/com/example/CatchingScammers/CatchingScammersApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CatchingScammersApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CatchingScammersApplication.class, args);
 	}
-
 }
