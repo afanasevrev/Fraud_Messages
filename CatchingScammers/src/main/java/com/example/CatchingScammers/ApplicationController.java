c6cb7c9ab381b3f6f95e148d1ce69e763f615077
revoly afanasev <rewoly@mail.ru>
2024-11-04 21:02:29 +0900

    Работа с формой

---
+++ CatchingScammers/src/main/java/com/example/CatchingScammers/ApplicationController.java
@@ -0,0 +1,12 @@
+package com.example.CatchingScammers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ApplicationController {
+    @GetMapping("/")
+    private String getInfo() {
+        return "form";
+    }
+}
