3d228289deacfc153ac7810f0c8cde718703cbe1
revoly afanasev <rewoly@mail.ru>
2024-11-04 22:08:39 +0900

    Работа с БД

--- CatchingScammers/src/main/java/com/example/CatchingScammers/FormController.java
+++ CatchingScammers/src/main/java/com/example/CatchingScammers/FormController.java
@@ -4,7 +4,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
 @RestController
 public class FormController {
     @PostMapping("/submit")
