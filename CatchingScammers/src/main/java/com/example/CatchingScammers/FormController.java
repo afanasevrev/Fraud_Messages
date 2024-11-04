dd5d397928579aa9531ed5f4570adcd06624bb15
revoly afanasev <rewoly@mail.ru>
2024-11-04 21:32:08 +0900

    Работа с БД

---
+++ CatchingScammers/src/main/java/com/example/CatchingScammers/FormController.java
@@ -0,0 +1,19 @@
+package com.example.CatchingScammers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FormController {
+    @PostMapping("/submit")
+    @ResponseBody
+    public String submitForm(@RequestParam String userInput) {
+        try {
+            return "Данные успешно отработаны";
+        } catch(Exception e) {
+            return "Ошибка при обработке данных";
+        }
+    }
+}
