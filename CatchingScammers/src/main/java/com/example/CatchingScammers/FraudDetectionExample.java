ab75ddcaefa6c26ec383755e7f709232890318cb
revoly afanasev <rewoly@mail.ru>
2024-11-05 23:13:56 +0900

    Работа с машинным обучением

---
+++ CatchingScammers/src/main/java/com/example/CatchingScammers/FraudDetectionExample.java
@@ -0,0 +1,15 @@
+package com.example.CatchingScammers;
+import java.util.ArrayList;
+import java.util.List;
+import smile.classification.LogisticRegression;
+import smile.data.vector.DoubleVector;
+import smile.math.MathEx;
+import smile.nlp.SimpleCorpus;
+//import smile.nlp.dictionary.SimpleStopwords;
+import smile.nlp.tokenizer.SimpleTokenizer;
+//import smile.nlp.transform.TfIdfVectorizer;
+//import smile.nlp.transform.TextTransform;
+
+public class FraudDetectionExample {
+
+}
