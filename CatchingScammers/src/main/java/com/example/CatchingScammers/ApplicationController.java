ae82d154b3cb4ff00db58d93530e6eacc7cf8d6c
revoly afanasev <rewoly@mail.ru>
2024-11-05 22:51:24 +0900

    Работа с машинным обучением

--- CatchingScammers/src/main/java/com/example/CatchingScammers/ApplicationController.java
+++ CatchingScammers/src/main/java/com/example/CatchingScammers/ApplicationController.java
@@ -2,7 +2,6 @@ package com.example.CatchingScammers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
 @Controller
 public class ApplicationController {
     @GetMapping("/")
