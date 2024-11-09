20bc677c681791132fdda285dd04c5310371691e
revoly afanasev <rewoly@mail.ru>
2024-11-09 23:30:57 +0900

    Обучаем модель

--- CatchingScammers/src/main/java/com/example/CatchingScammers/FraudDetector.java
+++ CatchingScammers/src/main/java/com/example/CatchingScammers/FraudDetector.java
@@ -14,17 +14,15 @@ public class FraudDetector {
     private static final String resource = "src/main/resources/static/model.txt";
     public static void main(String[] args) {
         try {
-            // Укажите путь к вашему тренировочному файлу с данными на русском языке
             InputStreamFactory dataIn = new InputStreamFactory() {
                 @Override
                 public FileInputStream createInputStream() throws IOException {
                     return new FileInputStream(resource);
                 }
             };
-            // Чтение и подготовка данных
             ObjectStream<String> lineStream = new PlainTextByLineStream(dataIn, StandardCharsets.UTF_8);
             ObjectStream<DocumentSample> sampleStream = new DocumentSampleStream(lineStream);
-            // Обучение модели
+
             DoccatModel model;
             try {
                 TrainingParameters params = new TrainingParameters();
@@ -36,14 +34,13 @@ public class FraudDetector {
             } finally {
                 sampleStream.close();
             }
-            // Использование модели
+
             DocumentCategorizerME categorizer = new DocumentCategorizerME(model);
-            // Пример сообщения для классификации
-            String message = "Приглашаем вас посетить открытие нового магазина в вашем городе в эту субботу. Будет много акций и скидок!";
+
+            String message = "";
             double[] outcomes = categorizer.categorize(new String[]{message});
             String category = categorizer.getBestCategory(outcomes);
             System.out.println("Категория сообщения: " + category);
-
         } catch (IOException e) {
             e.printStackTrace();
         }
