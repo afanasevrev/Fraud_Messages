db6d976b6ed76c85bc40ab4355cf038b398dd79f
revoly afanasev <rewoly@mail.ru>
2024-11-09 23:47:46 +0900

    Обучаем модель

--- CatchingScammers/src/main/java/com/example/CatchingScammers/FraudDetector.java
+++ CatchingScammers/src/main/java/com/example/CatchingScammers/FraudDetector.java
@@ -37,8 +37,10 @@ public class FraudDetector {
 
             DocumentCategorizerME categorizer = new DocumentCategorizerME(model);
 
-            String message = "";
-            double[] outcomes = categorizer.categorize(new String[]{message});
+            String message = "Сообщите нам код Вашей карты";
+            String[] message1 = message.split(" ");
+            System.out.println(message1.length);
+            double[] outcomes = categorizer.categorize(message1);
             String category = categorizer.getBestCategory(outcomes);
             System.out.println("Категория сообщения: " + category);
         } catch (IOException e) {
