1bfb80c3d219f6718f1932bced46f43124347cd3
afanasevrev <rewoly@mail.ru>
2024-11-13 00:33:19 +0900

    Проблема с чтением файла

--- CatchingScammers/src/main/java/com/example/CatchingScammers/FraudDetector.java
+++ CatchingScammers/src/main/java/com/example/CatchingScammers/FraudDetector.java
@@ -8,10 +8,12 @@ import opennlp.tools.util.PlainTextByLineStream;
 import opennlp.tools.util.TrainingParameters;
 
 import java.io.*;
+import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
 /**
  * Класс обучает модель и выдает предсказание на основе Байесовского алгоритма
  */
+/**
 public class FraudDetector {
     private static final String resource = "src/main/resources/static/model.txt";
     public static DoccatModel model;
@@ -45,3 +47,40 @@ public class FraudDetector {
         }
     }
 }
+ **/
+public class FraudDetector {
+    private static final String resource = "/static/model.txt"; // Используйте / для начала пути
+    public static DoccatModel model;
+
+    public void detector() {
+        try (InputStream inputStream = FraudDetector.class.getResourceAsStream(resource)) {
+            if (inputStream == null) {
+                throw new FileNotFoundException("Resource not found: " + resource);
+            }
+
+            // Используйте InputStream вместо FileInputStream напрямую
+            ObjectStream<String> lineStream = new PlainTextByLineStream(new InputStreamFactory() {
+                @Override
+                public InputStream createInputStream() throws IOException {
+                    return inputStream;
+                }
+            }, StandardCharsets.UTF_8);
+
+            ObjectStream<DocumentSample> sampleStream = new DocumentSampleStream(lineStream);
+            
+            try {
+                TrainingParameters params = new TrainingParameters();
+                params.put(TrainingParameters.ITERATIONS_PARAM, 100);
+                params.put(TrainingParameters.CUTOFF_PARAM, 1);
+                DoccatFactory factory = new DoccatFactory();
+                model = DocumentCategorizerME.train("ru", sampleStream, params, factory);
+                System.out.println("The training of the model was successful");
+            } finally {
+                sampleStream.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
