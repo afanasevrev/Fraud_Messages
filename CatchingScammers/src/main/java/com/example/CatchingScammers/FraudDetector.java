b54eb99df41ec71f700d1e5d118a61cd79d2165a
revoly afanasev <rewoly@mail.ru>
2024-11-13 22:53:22 +0900

    Удалили лишнее

--- CatchingScammers/src/main/java/com/example/CatchingScammers/FraudDetector.java
+++ CatchingScammers/src/main/java/com/example/CatchingScammers/FraudDetector.java
@@ -1,63 +1,23 @@
 package com.example.CatchingScammers;
 
 import opennlp.tools.doccat.*;
-import opennlp.tools.ml.naivebayes.NaiveBayesTrainer;
 import opennlp.tools.util.InputStreamFactory;
 import opennlp.tools.util.ObjectStream;
 import opennlp.tools.util.PlainTextByLineStream;
 import opennlp.tools.util.TrainingParameters;
-
 import java.io.*;
-import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
 /**
  * Класс обучает модель и выдает предсказание на основе Байесовского алгоритма
  */
-/**
-public class FraudDetector {
-    private static final String resource = "src/main/resources/static/model.txt";
-    public static DoccatModel model;
-    public void detector() {
-        InputStream inputStream = FraudDetector.class.getResourceAsStream(resource);
-        try {
-            InputStreamFactory dataIn = new InputStreamFactory() {
-                @Override
-                public FileInputStream createInputStream() throws IOException {
-                    return new FileInputStream(resource);
-                }
-            };
-            ObjectStream<String> lineStream = new PlainTextByLineStream(dataIn, StandardCharsets.UTF_8);
-            ObjectStream<DocumentSample> sampleStream = new DocumentSampleStream(lineStream);
-
-            //DoccatModel model;
-
-            try {
-                TrainingParameters params = new TrainingParameters();
-                params.put(TrainingParameters.ITERATIONS_PARAM, 100);
-                params.put(TrainingParameters.CUTOFF_PARAM, 1);
-                NaiveBayesTrainer trainer = new NaiveBayesTrainer();
-                DoccatFactory factory = new DoccatFactory();
-                model = DocumentCategorizerME.train("ru", sampleStream, params, factory);
-                System.out.println("The training of the model was successful");
-            } finally {
-                sampleStream.close();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-}
- **/
 public class FraudDetector {
     private static final String resource = "/static/model.txt"; // Используйте / для начала пути
     public static DoccatModel model;
-
     public void detector() {
         try (InputStream inputStream = FraudDetector.class.getResourceAsStream(resource)) {
             if (inputStream == null) {
                 throw new FileNotFoundException("Resource not found: " + resource);
             }
-
             // Используйте InputStream вместо FileInputStream напрямую
             ObjectStream<String> lineStream = new PlainTextByLineStream(new InputStreamFactory() {
                 @Override
@@ -65,9 +25,7 @@ public class FraudDetector {
                     return inputStream;
                 }
             }, StandardCharsets.UTF_8);
-
             ObjectStream<DocumentSample> sampleStream = new DocumentSampleStream(lineStream);
-            
             try {
                 TrainingParameters params = new TrainingParameters();
                 params.put(TrainingParameters.ITERATIONS_PARAM, 100);
